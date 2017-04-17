0d57e8b9365bdb31518dd2785dfde2307d7a6aca
rk <rkkk@gmail.com>
2017-04-17 15:44:55 +0000

    First Commit

---
+++ mavenProject/src/main/java/mavenProject/FirstMaven.java
@@ -0,0 +1,10 @@
+package mavenProject;
+
+public class FirstMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Maven");
+	}
+
+}
---
+++ mavenProject/src/test/java/mavenProject/FirstMavenTest.java
@@ -0,0 +1,14 @@
+package mavenProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstMavenTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
