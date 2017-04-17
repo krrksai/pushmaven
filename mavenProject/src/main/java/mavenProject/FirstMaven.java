d015546e378a59336ccd7759460a42652f509a63
rk <rkkk@gmail.com>
2017-04-17 15:55:01 +0000

    Second Commit

--- mavenProject/src/main/java/mavenProject/FirstMaven.java
+++ mavenProject/src/main/java/mavenProject/FirstMaven.java
@@ -5,6 +5,7 @@ public class FirstMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Maven");
+		System.out.println("Second commit");
 	}
 
 }
